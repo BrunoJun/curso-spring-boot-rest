ca6396cc2ca78b593eb9f4f8b2635c631085b680
BrunoJun <brunojun7@gmail.com>
2023-11-28 16:05:30 -0300

    Integra tela de cadastro com a API

--- client/src/pages/newPerson/Index.js
+++ client/src/pages/newPerson/Index.js
@@ -1,12 +1,57 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { useNavigate } from 'react-router-dom';
+import api from '../../services/Api';
+
 import logo from '../../assets/logo.svg'
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi'
 
 import './styles.css';
 
-
 export default function NewPerson(){
+
+    const [id, setId] = useState(null);
+    const [firstName, setFirstName] = useState(''); 
+    const [lastName, setLastName] = useState('');
+    const [address, setAddress] = useState('');
+    const [gender, setGender] = useState('');
+    const [enabled, setEnable] = useState(false);
+
+    const navigate = useNavigate();
+
+    const accessToken = localStorage.getItem('accessToken')
+
+    async function createNewPerson(e){
+
+        e.preventDefault();
+
+        const data = {
+
+            firstName,
+            lastName,
+            address,
+            gender,
+            enabled
+        }
+
+        try {
+
+            console.log(data)
+            
+            await api.post('api/person/v1', data,
+
+            {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            });
+
+            navigate('/person');
+        } catch (error) {
+            
+            alert('Error while recording person!');
+        }
+    }
 
     return(
 
@@ -22,11 +67,12 @@ export default function NewPerson(){
                     </Link>
                 </section>
 
-                <form>
-                    <input placeholder='First Name'/>
-                    <input placeholder='Last Name'/>
-                    <input placeholder='Address'/>
-                    <input placeholder='Gender'/>
+                <form onSubmit={createNewPerson}>
+                    <input placeholder='First Name' value={firstName} onChange={e => setFirstName(e.target.value)}/>
+                    <input placeholder='Last Name' value={lastName} onChange={e => setLastName(e.target.value)}/>
+                    <input placeholder='Address' value={address} onChange={e => setAddress(e.target.value)}/>
+                    <input placeholder='Gender' value={gender} onChange={e => setGender(e.target.value)}/>
+                    <input placeholder='Enabled' value={enabled} onChange={e => setEnable(e.target.value)}/>
 
                     <button className='button' type='submit'>Add</button>
                 </form>
